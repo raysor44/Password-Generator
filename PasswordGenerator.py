21570ee766392abff1b14005040360a045a62aa7
raysor44 <panserekmleczaty@gmail.com>
2020-12-18 22:46:27 +0100

    Squashed commit.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,30 +1,101 @@
+import argparse
 import random
 import string
+import sys
 
-# This script automatically generates password of 15 characters length.
+from termcolor import colored
+from colorama import init 
 
-specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
+##################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--exclude_numbers", action="store_true", help="Exclude Numbers")
+parser.add_argument("-ll", "--exclude_lletters", action="store_true", help="Exclude Lowercase Letters")
+parser.add_argument("-ul", "--exclude_uletters", action="store_true", help="Exclude Uppercase Letters")
+parser.add_argument("-sc", "--exclude_scharacters", action="store_true", help="Exclude Special Characters")
+args = parser.parse_args()
+
+##################################
+
+
+def askForPasswordLength():
+    global passwordLength
+
+    try:
+        passwordLength = int(input("How much characters you want to be in your password: "))
+    except ValueError:
+        print("Invalid Value, try again"); sys.exit(0)
+
 
 class Generating:
-    def GenerateRandomNumbers(self):
-        global generatedNumbers
+    def randomNumbers(self):
+        global usedNumbers
+        usedNumbers = str(random.randint(0, 1000 * 1000))
 
-        generatedNumbers = str(random.randint(0, 1000 * 1000))
-        print("Generated Numbers:", generatedNumbers)
+        if args.exclude_numbers:
+            print("\nUsed Numbers:", colored("None (Disabled)", "red"))
+        else: 
+            print("\nUsed Numbers:", colored(usedNumbers, "yellow"))
 
-    def GenerateRandomLetters(self):
-        global generatedLetters
+    def randomLowercaseLetters(self):
+        global usedLowercaseLetters
+        usedLowercaseLetters = string.ascii_lowercase
 
-        generatedLetters = string.ascii_letters
-        print("Generated Letters:", generatedLetters)
+        if args.exclude_lletters:
+            print("Used Lowercase Letters:", colored("None (Disabled)", "red"))
+        else:
+            print("Used Lowercase Letters:", colored(usedLowercaseLetters, "yellow"))
+        
+    def randomUppercaseLetters(self):
+        global usedUppercaseLetters
+        usedUppercaseLetters = string.ascii_uppercase
 
-    def GeneratePassword(self):
-        result = "".join(random.choice(generatedNumbers) + random.choice(generatedLetters) + random.choice(specialCharacters) for i in range (0, 5))
+        if args.exclude_uletters:
+            print("Used Uppercase Letters:", colored("None (Disabled)", "red"))
+        else:
+            print("Used Uppercase Letters:", colored(usedUppercaseLetters, "yellow"))
 
-        print("Generated Password:", result)
+    def randomSpecialCharacters(self):
+        global usedSpecialCharacters
+        usedSpecialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"]        
 
+        if args.exclude_scharacters:
+            print("Used Special Characters:", colored("None (Disabled)", "red"))
+        else:
+            print("Used Special Characters:", colored(usedSpecialCharacters, "yellow"))
+
+
+    def generatePassword(self):
+        choices = [usedNumbers, usedLowercaseLetters, usedUppercaseLetters, usedSpecialCharacters]
+
+        if args.exclude_numbers:
+            choices.remove(usedNumbers)
+        
+        if args.exclude_lletters:
+            choices.remove(usedLowercaseLetters)
+            
+        if args.exclude_uletters:
+            choices.remove(usedUppercaseLetters)
+        
+        if args.exclude_scharacters:
+            choices.remove(usedSpecialCharacters)
+
+        if len(choices) == 0: # Check if every item in 'choices' list is excluded.
+            print("Password will not be generated"); sys.exit(0)
+
+        print("Generated Password:", end=" ")
+
+        for i in range(passwordLength):
+            print(random.choice(random.choice(choices)), end="")
+
+
+init() # Initialisation of Colorama Module
+askForPasswordLength()
 
 p1 = Generating()
-p1.GenerateRandomNumbers()
-p1.GenerateRandomLetters()
-p1.GeneratePassword()
+p1.randomNumbers()
+p1.randomLowercaseLetters()
+p1.randomUppercaseLetters()
+p1.randomSpecialCharacters()
+p1.generatePassword()
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Password Generator, Feel free to use if you want to
+# Password Generator
+Simple password generator created in python, password generator doesn't have any characters amount limit.
+Password is generated using letters, numbers and special characters in random positions.
+
+# Required Modules
+Password Generator requires some not default installed modules to work, you can install those modules with these commands.
+``` python
+pip install termcolor
+pip install colorama
+```
+
+# Arguments
+|Short Argument|Full Argument            |Description                                  |
+|--------------|-------------------------|---------------------------------------------|
+|-h            |--help                   |Shows help message                           |
+|-n            |--exclude_numbers        |Excludes Numbers from Password               |
+|**-ll**       |**--exclude_lletters**   |Excludes **Lowercase** Letters from Password |
+|**-ul**       |**--exclude_uletters**   |Excludes **Uppercase** Letters from Password |
+|-sc           |--exclude_scharacters    |Excludes Special Characters from Password    |
+
+# How to run and use
+1. Run: 'python PasswordGenerator.py' in terminal
+2. Password Generator will now ask you to type length of your password, you can use any value you want.
+3. Your password will be displayed alongside with letters, numbers and special characters used in generating password.
